d9c8b98e31daa526ea32d2ac5df775a02963210e
Samuel Roberti <samuel.p.roberti@gmail.com>
2020-06-29 19:15:46 -0400

    Add preset layouts

---
+++ desktop.lisp
@@ -0,0 +1,137 @@
+#S(DDUMP
+   :SCREENS (#S(SDUMP
+                :NUMBER 0
+                :GROUPS (#S(GDUMP
+                            :NUMBER 1
+                            :NAME "Default"
+                            :TREE (#S(FDUMP
+                                      :NUMBER 2
+                                      :X 2560
+                                      :Y 0
+                                      :WIDTH 2560
+                                      :HEIGHT 1440
+                                      :WINDOWS NIL
+                                      :CURRENT NIL)
+                                   #S(FDUMP
+                                      :NUMBER 3
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 2560
+                                      :HEIGHT 1440
+                                      :WINDOWS NIL
+                                      :CURRENT NIL))
+                            :CURRENT 2)
+                         #S(GDUMP
+                            :NUMBER 2
+                            :NAME "Development"
+                            :TREE (#S(FDUMP
+                                      :NUMBER 2
+                                      :X 2560
+                                      :Y 0
+                                      :WIDTH 2560
+                                      :HEIGHT 1440
+                                      :WINDOWS (29360129)
+                                      :CURRENT 29360129)
+                                   #S(FDUMP
+                                      :NUMBER 3
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 2560
+                                      :HEIGHT 1440
+                                      :WINDOWS (8389921 35651585)
+                                      :CURRENT 8389921))
+                            :CURRENT 3)
+                         #S(GDUMP
+                            :NUMBER 3
+                            :NAME "Procrastination"
+                            :TREE (#S(FDUMP
+                                      :NUMBER 2
+                                      :X 2560
+                                      :Y 0
+                                      :WIDTH 2560
+                                      :HEIGHT 1440
+                                      :WINDOWS (27262977)
+                                      :CURRENT 27262977)
+                                   #S(FDUMP
+                                      :NUMBER 3
+                                      :X 0
+                                      :Y 0
+                                      :WIDTH 2560
+                                      :HEIGHT 1440
+                                      :WINDOWS (48234504)
+                                      :CURRENT 48234504))
+                            :CURRENT 3)
+                         #S(GDUMP
+                            :NUMBER 4
+                            :NAME "Communication"
+                            :TREE (#S(FDUMP
+                                      :NUMBER 2
+                                      :X 2560
+                                      :Y 0
+                                      :WIDTH 2560
+                                      :HEIGHT 1440
+                                      :WINDOWS (29360139 29360136 46137347)
+                                      :CURRENT 29360139)
+                                   (#S(FDUMP
+                                       :NUMBER 3
+                                       :X 0
+                                       :Y 0
+                                       :WIDTH 1280
+                                       :HEIGHT 1440
+                                       :WINDOWS (8388636)
+                                       :CURRENT 8388636)
+                                    #S(FDUMP
+                                       :NUMBER 0
+                                       :X 1280
+                                       :Y 0
+                                       :WIDTH 1280
+                                       :HEIGHT 1440
+                                       :WINDOWS NIL
+                                       :CURRENT NIL)))
+                            :CURRENT 2)
+                         #S(GDUMP
+                            :NUMBER 5
+                            :NAME "System"
+                            :TREE ((#S(FDUMP
+                                       :NUMBER 2
+                                       :X 2560
+                                       :Y 0
+                                       :WIDTH 1280
+                                       :HEIGHT 1440
+                                       :WINDOWS (6291503)
+                                       :CURRENT 6291503)
+                                    #S(FDUMP
+                                       :NUMBER 4
+                                       :X 3840
+                                       :Y 0
+                                       :WIDTH 1280
+                                       :HEIGHT 1440
+                                       :WINDOWS NIL
+                                       :CURRENT NIL))
+                                   ((#S(FDUMP
+                                        :NUMBER 3
+                                        :X 0
+                                        :Y 0
+                                        :WIDTH 853
+                                        :HEIGHT 1440
+                                        :WINDOWS (6291523)
+                                        :CURRENT 6291523)
+                                     #S(FDUMP
+                                        :NUMBER 1
+                                        :X 853
+                                        :Y 0
+                                        :WIDTH 853
+                                        :HEIGHT 1440
+                                        :WINDOWS NIL
+                                        :CURRENT NIL))
+                                    #S(FDUMP
+                                       :NUMBER 0
+                                       :X 1706
+                                       :Y 0
+                                       :WIDTH 854
+                                       :HEIGHT 1440
+                                       :WINDOWS (6291513)
+                                       :CURRENT 6291513)))
+                            :CURRENT 3))
+                :CURRENT 5))
+   :CURRENT 0)
---
+++ layout.lisp
@@ -0,0 +1,13 @@
+(("System" 3 T T :CLASS "URxvt" :INSTANCE "urxvt" :TITLE "stumpish" :ROLE NIL)
+ ("System" 2 T T :CLASS "URxvt" :INSTANCE "urxvt" :TITLE "ncmpcpp" :ROLE NIL)
+ ("System" 0 T T :CLASS "URxvt" :INSTANCE "urxvt" :TITLE "gotop" :ROLE NIL)
+ ("Communication" 3 T T :CLASS "Slack" :INSTANCE "slack" :TITLE NIL :ROLE
+  "browser-window")
+ ("Procrastination" 3 T T :CLASS "discord" :INSTANCE "discord" :TITLE NIL :ROLE
+  "browser-window")
+ ("Development" 3 T T :CLASS "Emacs" :INSTANCE "emacs" :TITLE NIL :ROLE NIL)
+ ("Development" 2 T T :CLASS "Next" :INSTANCE "next" :TITLE NIL :ROLE NIL)
+ ("Procrastination" 2 T T :CLASS "Chromium-browser" :INSTANCE
+  "chromium-browser" :TITLE "YouTube - Chromium" :ROLE "browser")
+ ("Communication" 2 T T :CLASS "Chromium-browser" :INSTANCE "chromium-browser"
+  :TITLE "Mail - Samuel Roberti - Outlook - Chromium" :ROLE "browser"))
